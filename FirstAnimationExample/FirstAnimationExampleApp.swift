92746b7596231eb21cd35356154187dcee20712c
Nathan Smith <nsmith@lcs.on.ca>
2022-04-21 11:21:14 -0400

    Initial Commit

---
+++ FirstAnimationExample/FirstAnimationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAnimationExampleApp.swift
+//  FirstAnimationExample
+//
+//  Created by Nathan Smith on 2022-04-21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAnimationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
